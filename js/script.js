b249fb1973f406922e4105b83e413008825cb135
Noiker-ux <71761947+Noiker-ux@users.noreply.github.com>
2021-02-21 11:33:01 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,62 @@
+$(function () {
+    $("a[href^='#Services']").click(function () {
+        var _href = $(this).attr("href");
+        $("html, body").animate({
+            scrollTop: $(_href).offset().top + "px"
+        });
+        return false;
+    });
+});
+
+$(function () {
+    $("a[href^='#Team']").click(function () {
+        var _href = $(this).attr("href");
+        $("html, body").animate({
+            scrollTop: $(_href).offset().top + "px"
+        });
+        return false;
+    });
+});
+
+
+$(function () {
+    $("a[href^='#Skills']").click(function () {
+        var _href = $(this).attr("href");
+        $("html, body").animate({
+            scrollTop: $(_href).offset().top + "px"
+        });
+        return false;
+    });
+});
+
+
+$(function () {
+    $("a[href^='#Portfolio']").click(function () {
+        var _href = $(this).attr("href");
+        $("html, body").animate({
+            scrollTop: $(_href).offset().top + "px"
+        });
+        return false;
+    });
+});
+
+
+$(function () {
+    $("a[href^='#Comments']").click(function () {
+        var _href = $(this).attr("href");
+        $("html, body").animate({
+            scrollTop: $(_href).offset().top + "px"
+        });
+        return false;
+    });
+});
+
+$(function () {
+    $("a[href^='#Contacts']").click(function () {
+        var _href = $(this).attr("href");
+        $("html, body").animate({
+            scrollTop: $(_href).offset().top + "px"
+        });
+        return false;
+    });
+});
